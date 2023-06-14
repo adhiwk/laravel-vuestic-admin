4b8c3217ac76b64cc42b38dff961159dd1a72349
Musriadi M <info@indovb.com>
2023-06-15 00:29:35 +0800

    Project starting

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link  href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,400;1,700&display=swap" rel="stylesheet"/>
+        <link  href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
+        @vite('resources/js/main.ts')
+    </head>
+<body class="antialiased">
+    <div id="app"></div> 
+</body>
+</html>
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.{vue,js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
