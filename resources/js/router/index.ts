808a36afc1ed8e9b9ecd86cb3433abf4bf268b3e
Musriadi M <info@indovb.com>
2023-06-18 22:49:05 +0800

    update vue router

--- resources/js/router/index.ts
+++ resources/js/router/index.ts
@@ -266,8 +266,12 @@ const routes: Array<RouteRecordRaw> = [
 ];
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    //  mode: process.env.VUE_APP_ROUTER_MODE_HISTORY === 'true' ? 'history' : 'hash',
+    history: createWebHistory(),
+    /**
+     * if you want show default build path 
+     * use createWebHistory(import.meta.env.BASE_URL)
+     * mode: process.env.VUE_APP_ROUTER_MODE_HISTORY === 'true' ? 'history' : 'hash',
+     */
     routes,
 });
 
